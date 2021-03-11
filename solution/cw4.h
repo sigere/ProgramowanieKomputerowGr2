cb1f0afbc477133b5d17f95c7b7814162b2abf98
Krzysztof Marczak <a.marczak1324@gmail.com>
2021-03-11 10:43:03 +0100

    rename solution folder

---
+++ solution/cw4.h
@@ -0,0 +1,15 @@
+#pragma once
+#pragma once
+int czy_doskonala(int a);
+void czy_doskonala_test();
+void czy_n_doskonale();
+void wypisz_liczby2();
+void ciagn();
+void fibonacci();
+void maks_n();
+void nwd();
+void najwiekszen();
+void najwiekszen2();
+void dec2u2();
+void print_digits();
+void repair(int* tab);
