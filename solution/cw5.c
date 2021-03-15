c49f6fd6116a01045019182c9e098c7018085fd9
Krzysztof Marczak <a.marczak1324@gmail.com>
2021-03-15 18:57:37 +0100

    cw5 zad4

--- solution/cw5.c
+++ solution/cw5.c
@@ -33,12 +33,28 @@ int reverseAndAdd(int a)
 
 void startedWith(char c, char* str)
 {
-	char* tmp = NULL;
+	char* tmp = NULL; //pomocnicza zmienna wymagana przez strtok_s
 	char* token = strtok_s(str, " ,\t\n", &tmp);
-	while(token)
+	while (token)
 	{
 		if ((char)tolower(token[0]) == c)
 			printf("%s ", token);
 		token = strtok_s(NULL, " ,\t\n", &tmp);
 	}
+	printf("\n");
+}
+
+void cipher(char* text)
+{
+	char tmp[100]; //pomocnicza zmienna, do wygenerowania klucza
+	int key[100]; // klucz sk³adaj¹cy siê z kolejno: ostatniej, pierwszej, drugiej.. n-1-tej
+				  // litery szyfrowanego tekstu (finalnie o takiej samej d³ugoœci co text)
+	int index = sprintf_s(tmp, 100, "%c%s", text[strlen(text) - 1], text) - 1; // d³ugoœæ textu
+	for (int i = 0; i < index; i++)
+		key[i] = toupper(tmp[i]) - 'A';
+
+	for (int i = 0; i < index; i++) // przestawienie 
+		text[i] = (((toupper(text[i]) - 'A' + key[i])) % ('Z' - 'A')) + 'A';
+
+	printf("%s\n", text);
 }
--- solution/main.c
+++ solution/main.c
@@ -8,13 +8,15 @@
 
 int main()
 {
-	printf("%s\n",checkIfPalindrome(101) ? "true" : "false");
-	printf("%s\n",checkIfPalindrome(1101) ? "true" : "false");
-	printf("%s\n",checkIfPalindrome(1223221) ? "true" : "false");
-	printf("%s\n",checkIfPalindrome(1) ? "true" : "false");
-	printf("%s\n",checkIfPalindrome(111) ? "true" : "false");
+	printf("%s\n", checkIfPalindrome(101) ? "true" : "false");
+	printf("%s\n", checkIfPalindrome(1101) ? "true" : "false");
+	printf("%s\n", checkIfPalindrome(1223221) ? "true" : "false");
+	printf("%s\n", checkIfPalindrome(1) ? "true" : "false");
+	printf("%s\n", checkIfPalindrome(111) ? "true" : "false");
 
 	printf("%d\n", reverseAndAdd(195));
-	char string1[] ="ala Algieria alabama niea Ataman";
+	char string1[] = "ala Algieria alabama niea Ataman";
 	startedWith('a', string1);
+	char string2[] = "abcdefghijklmnopqrstuvwxyz";
+	cipher(string2);
 }
