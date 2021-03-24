24fd08acc71263f135efb7b6fe679332f105fc75
Krzysztof Marczak <a.marczak1324@gmail.com>
2021-03-24 16:09:23 +0100

    cw8

--- solution/cw8.c
+++ solution/cw8.c
@@ -13,6 +13,44 @@ int fibbonacii(int n) {
 	return fibbonacii(n - 1) + fibbonacii(n - 2);
 }
 
+float bisection(float(*function)(float),float precision, int a, int b)
+{
+	if (a > b)
+	{
+		int tmp = a;
+		a = b;
+		b = tmp;
+	}
+
+	if ((*function)(a) * (*function)(b) > 0)
+		return -1.0;
+	float val = (*function)((b + a) / 2);
+	printf("val: %f\n", val);
+	if (abs(val) < precision) {
+		printf("%d, %d\n", a, b);
+		return (b + a) / 2.0;
+	}
+	return val ? bisection(function,precision, (b + a) / 2, b) : bisection(function, precision, a, (b + a) / 2);
+}
+
+char* to_binary(int dec)
+{
+	if (dec == 0)
+		return malloc(255 * sizeof(char));
+	char digit = dec % 2 + '0';
+	dec /= 2;
+	return strcat(to_binary(dec),&digit);
+}
+
+int first_capital(char* text, int size, int n)
+{
+	if (n >= size)
+		return -1;
+	if ('A' <= text[n] && text[n] <= 'Z')
+		return n;
+	return first_capital(text, size, n+1);
+}
+
 void hanoi(int n, char x, char y, char z, FILE* fp)
 {
 	if (n == 1)
