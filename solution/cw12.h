508350030971349eb59765c300cc8ca9d203587b
Sigere <a.marczak1324@gmail.com>
2021-04-19 14:35:44 +0200

    cw12

--- solution/cw12.h
+++ solution/cw12.h
@@ -1,1 +1,5 @@
 #pragma once
+struct node* getMax(struct node* root);
+struct node* getMin(struct node* root);
+void removeNode(int value, struct node* root);
+void insertNode(int value, struct node* root);
